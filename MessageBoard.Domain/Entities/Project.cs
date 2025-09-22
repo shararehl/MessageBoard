f63beda9db8e19f3cd761dc59cdabc7b5ade36d6
shara <shara@DOMINO>
2025-09-22 10:26:51 +1000

    Add project files.

---
+++ MessageBoard.Domain/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using MessageBoard.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBoard.Domain.Entities
+{
+    public class Project:BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MessageBoard.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using MessageBoard.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBoard.Domain.Entities
+{
+    public class User:BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
